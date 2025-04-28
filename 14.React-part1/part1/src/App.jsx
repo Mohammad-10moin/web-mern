bbd7de8eb1a1707d77c1a2220e9c5e1eac0e4f84
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-04-28 17:24:51 +0530

    restarting react from basics

--- 14.React-part1/part1/src/App.jsx
+++ 14.React-part1/part1/src/App.jsx
@@ -4,8 +4,9 @@ function App() {
     <>
     {/* Hot Module Replacement : if we make any changes in our code then it automatically appears in frontend  */}
       <div>
-        Hello Moin 
+        Hello Moin 10
       </div>
+      {/* npm run dev -- we are running a development server */}
     </>
   )
 }
