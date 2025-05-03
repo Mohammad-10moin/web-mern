cbecbbd5ec8120ad1cbef2b4acd47763542ab452
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-05-03 20:27:22 +0530

    learnt creating components

--- 14.React-part1/part1/src/App.jsx
+++ 14.React-part1/part1/src/App.jsx
@@ -1,32 +1,39 @@
 
 function App() {
   return (
-    <>
-    <PostComponent/>
-    {/* Hot Module Replacement : if we make any changes in our code then it automatically appears in frontend  */}
-      <div>
-        Hello Moin 10
+    <div style={{backgroundColor:"#dfe6e9",height:"100vh",display:"flex",justifyContent:"center"}}>
+      <div style={{alignItems:"center"}}>
+        <div>
+          <PostComponent/>
+        </div>
+        <div>
+          <PostComponent/>
+        </div>
+        <div>
+          <PostComponent/>
+        </div>
       </div>
-      {/* npm run dev -- we are running a development server */}
-    </>
+    </div>
   )
 }
 
 
 function PostComponent(){
   return(
-    <>
-      <div style={{ width:"500px",height:"550px" , backgroundColor:"ghostwhite", display:"flex" }}>
-          <img src="cake.jpg" alt="" style={{width:50,height:50,border:"1px solid grey",  borderRadius:50}}/>
-          <div>
-            <div style={{display:"flex"}}>
-              <p style={{fontSize:18,fontWeight:600, marginLeft:5}}>Mohammad </p>
-              <p style={{fontSize:12, color:"grey", alignContent:"center",marginLeft:2}}> . Following</p>
-            </div>
-              <p style={{fontSize:12, color:"grey"}}>Software Engineer at Google</p>
+    <div style={{height:300,width:480,border:"1px solid grey",borderRadius:8,backgroundColor:"white",marginBottom:10}}>
+      <div style={{margin:15}}>
+        <div style={{display:"flex"}}>
+          <img src="cake.jpg" style={{ height:50,width:50,borderRadius:50,border:"1px solid grey"}}/>
+          <div style={{marginLeft:15}}>
+            <b>Careers.dev</b>
+            <div>12,345 followers</div>
+            <div>15m</div>
           </div>
+        </div>
+        <br />
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nesciunt similique laudantium debitis, modi perspiciatis   tempora quo ad id nam, deserunt aut quis explicabo sapiente sunt, fugit officiis iste. Provident?</div>
       </div>
-    </>
+    </div>
   )
 }
 
