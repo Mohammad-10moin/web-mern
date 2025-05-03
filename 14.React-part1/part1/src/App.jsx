d05beded5fc6728d4a3f4c067b6148a9271c7a93
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-05-03 17:20:24 +0530

    initial react commit

--- 14.React-part1/part1/src/App.jsx
+++ 14.React-part1/part1/src/App.jsx
@@ -2,6 +2,7 @@
 function App() {
   return (
     <>
+    <PostComponent/>
     {/* Hot Module Replacement : if we make any changes in our code then it automatically appears in frontend  */}
       <div>
         Hello Moin 10
@@ -10,5 +11,27 @@ function App() {
     </>
   )
 }
+
+
+function PostComponent(){
+  return(
+    <>
+      <div style={{ width:"500px",height:"550px" , backgroundColor:"ghostwhite", display:"flex" }}>
+          <img src="cake.jpg" alt="" style={{width:50,height:50,border:"1px solid grey",  borderRadius:50}}/>
+          <div>
+            <div style={{display:"flex"}}>
+              <p style={{fontSize:18,fontWeight:600, marginLeft:5}}>Mohammad </p>
+              <p style={{fontSize:12, color:"grey", alignContent:"center",marginLeft:2}}> . Following</p>
+            </div>
+              <p style={{fontSize:12, color:"grey"}}>Software Engineer at Google</p>
+          </div>
+      </div>
+    </>
+  )
+}
+
+
+
+
 
 export default App
