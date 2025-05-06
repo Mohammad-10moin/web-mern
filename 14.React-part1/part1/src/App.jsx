95b83e51d5abbb57bad623201b6f9d2f343fc3b9
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-05-06 22:13:04 +0530

    created add post button--adds posts

--- 14.React-part1/part1/src/App.jsx
+++ 14.React-part1/part1/src/App.jsx
@@ -2,20 +2,36 @@ import { useState } from "react"
 import { PostComponent } from "./Post"
 
 function App() {
-  return (
-      <div style={{backgroundColor:"#dfe6e9",height:"100%" ,display:"flex",justifyContent:"center"}}>
-        <div> <button onClick={()=>{
+  
+  const [posts,setpost]=useState([])
 
-        }}>Add Post</button></div>
-        
-        <div style={{alignItems:"center"}}>
+  const postComponents=posts.map(post=><PostComponent
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    image={post.image}
+    description={post.description}
+    />
+  )
+
+  function addpost(){
+    setpost([...posts,{
+      name:"CakeBuddy",
+      subtitle:"12,345 followers",
+      time:"12m ago",
+      image:"/cake.jpg",
+      description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,   tenetur       nesciunt iste, dolores enim   incidunt assumenda facere mollitia  asperiores rerum       adipisci explicabo natus, corrupti ipsum eveniet saepe!   Vel,  officia aspernatur!",
+    }])
+  }
+
+  return (
+      <div style={{backgroundColor:"#dfe6e9",height:"100%" }}>
+
+        <div> <button onClick={addpost}>Add Post</button></div>
+
+        <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
           <div>
-            <PostComponent
-              name={"CakeBuddy"}
-              subtitle={"12,345 followers"}
-              image={"/cake.jpg"}
-              description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,   tenetur       nesciunt iste, dolores enim   incidunt assumenda facere mollitia  asperiores rerum       adipisci explicabo natus, corrupti ipsum eveniet saepe!   Vel,  officia aspernatur!"}
-            />
+            {postComponents}
           </div>  
         </div>
       </div>
