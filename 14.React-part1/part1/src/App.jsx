c7544702cb75908c138a5e55fdfa171e5759fbeb
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-05-07 06:32:24 +0530

    understood the issue with setInterval in case of re-rendering --useState

--- 14.React-part1/part1/src/App.jsx
+++ 14.React-part1/part1/src/App.jsx
@@ -3,39 +3,57 @@ import { PostComponent } from "./Post"
 
 function App() {
   
-  const [posts,setpost]=useState([])
+  const [count,setcount]=useState(0);
 
-  const postComponents=posts.map(post=><PostComponent
-    name={post.name}
-    subtitle={post.subtitle}
-    time={post.time}
-    image={post.image}
-    description={post.description}
-    />
-  )
-
-  function addpost(){
-    setpost([...posts,{
-      name:"CakeBuddy",
-      subtitle:"12,345 followers",
-      time:"12m ago",
-      image:"/cake.jpg",
-      description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,   tenetur       nesciunt iste, dolores enim   incidunt assumenda facere mollitia  asperiores rerum       adipisci explicabo natus, corrupti ipsum eveniet saepe!   Vel,  officia aspernatur!",
-    }])
+  function inc(){
+    setcount(count+1)
   }
+  // setInterval(() => {
+  //   setcount(count+1)
+  // }, 1000);
+  
+  console.log("re-rendering");
 
   return (
-      <div style={{backgroundColor:"#dfe6e9",height:"100%" }}>
-
-        <div> <button onClick={addpost}>Add Post</button></div>
-
-        <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-          <div>
-            {postComponents}
+      <div style={{backgroundColor:"#dfe6e9",height:"100%",marginTop:30 }}>
+        {/* <div> <button onClick={inc}>increase count</button></div> */}
+          <div style={{backgroundColor:"red",color:"black",width:20,height:20,border:"1px solid black", borderRadius:20,display:"flex",justifyContent:"center"}}>
+            {count}
           </div>  
-        </div>
       </div>
   )
 }
+
+
+
+
+
+
+        {/* <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}> */}
+        {/* </div> */}
+
+        {/* <div> <button onClick={addpost}>Add Post</button></div> */}
+
+
+  // const [posts,setpost]=useState([])
+
+  // const postComponents=posts.map(post=><PostComponent
+  //   name={post.name}
+  //   subtitle={post.subtitle}
+  //   time={post.time}
+  //   image={post.image}
+  //   description={post.description}
+  //   />
+  // )
+
+  // function addpost(){
+  //   setpost([...posts,{
+  //     name:"CakeBuddy",
+  //     subtitle:"12,345 followers",
+  //     time:"12m ago",
+  //     image:"/cake.jpg",
+  //     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,   tenetur       nesciunt iste, dolores enim   incidunt assumenda facere mollitia  asperiores rerum       adipisci explicabo natus, corrupti ipsum eveniet saepe!   Vel,  officia aspernatur!",
+  //   }])
+  // }
 
 export default App
