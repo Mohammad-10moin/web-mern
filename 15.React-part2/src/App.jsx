78099c0bf5a5ad270ee17fc90b91d631cdf2104d
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-05-11 18:15:14 +0530

    understood list and keys and importance of having keys and downside of not having it

--- 15.React-part2/src/App.jsx
+++ 15.React-part2/src/App.jsx
@@ -1,27 +1,50 @@
 import { useState } from 'react'
 
 function App() {
-// Here now lets understand children in react 
+// Now let's understand list and keys 
+// whenever we render components inside the list then the list needs to have a unique key 
+// react understand based on the key i.e. if the component with key1 goes below and key2 goes up then the react simply swaps them instead of removing both of them and again adding them 
   return (
-    <div style={{display:"flex"}}>
-      <Card>
-        <div style={{ color:"red",fontWeight:600}}>Hi there</div>
-      </Card>
-      <Card>
-        <div>
-          Enter you text here:
-          <br />
-          <input type="text" />
-        </div>
-      </Card>
+    <div>
+      {[
+        <Todo key={1} title={"todo one"} done={false}/>,
+        <Todo key={2} title={"todo two"} done={true}/>
+      ]}
     </div>
   )
 }
 
-function Card({children}){
+
+function Todo({title,done}){
   return(
-    <div style={{ backgroundColor: 'grey',padding:20,margin:20, border:"1px dashed black",borderRadius:20,}}>{children}</div>
+    <div>
+      {title} --{done? "Done":"not done"}
+    </div>
   )
 }
+
+
+
+
+
+
+      // <Card>
+      //   <div style={{ color:"red",fontWeight:600}}>Hi there</div>
+      // </Card>
+      // <Card>
+      //   <div>
+      //     Enter you text here:
+      //     <br />
+      //     <input type="text" />
+      //   </div>
+      // </Card>
+
+
+
+// function Card({children}){
+//   return(
+//     <div style={{ backgroundColor: 'grey',padding:20,margin:20, border:"1px dashed black",borderRadius:20,}}>{children}</div>
+//   )
+// }
 
 export default App
