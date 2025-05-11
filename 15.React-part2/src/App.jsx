2b3ef5d4c4ee9d5edfb74c2b6c4bbc224338a939
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-05-11 17:30:51 +0530

    learnt children property

--- 15.React-part2/src/App.jsx
+++ 15.React-part2/src/App.jsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+// Here now lets understand children in react 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{display:"flex"}}>
+      <Card>
+        <div style={{ color:"red",fontWeight:600}}>Hi there</div>
+      </Card>
+      <Card>
+        <div>
+          Enter you text here:
+          <br />
+          <input type="text" />
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+function Card({children}){
+  return(
+    <div style={{ backgroundColor: 'grey',padding:20,margin:20, border:"1px dashed black",borderRadius:20,}}>{children}</div>
   )
 }
 
