ca1e1502d7a773e3d90023008b6b4b4445312f26
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-06-01 23:51:59 +0530

    completed flex and grid in tailwind

--- 21.Tailwind/src/App.jsx
+++ 21.Tailwind/src/App.jsx
@@ -18,6 +18,18 @@ function App() {
         <div className='bg-blue-400 col-span-3'>Grid2</div>
         <div className='bg-green-400 col-span-3'>Grid3</div>
       </div>
+      <p className='text-red-500'>Let's understand responsiveness</p>
+      <div className='sm:bg-red-300 bg-amber-300'>Understanding Responsiveness implementation in tailwind</div>
+      <div className='sm:flex'>
+        <div className='bg-orange-400 sm:flex-1/2'>child1</div>
+        <div className='bg-blue-400 sm:flex-1/4'>child2</div>
+        <div className='bg-green-400 sm:flex-1/4'>child3</div>
+      </div>
+      <div className='grid sm:grid-cols-12 grid-cols-1'>
+        <div className='bg-orange-400 sm:col-span-6'>Grid1</div>
+        <div className='bg-blue-400 sm:col-span-3'>Grid2</div>
+        <div className='bg-green-400 sm:col-span-3'>Grid3</div>
+      </div>
     </>
   )
 }
