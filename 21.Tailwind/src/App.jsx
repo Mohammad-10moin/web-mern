094b8e0b4fdcc206ad3d7b75ed2eb36f2203c8b9
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-06-01 17:53:12 +0530

    completed flex and grid in tailwind

--- 21.Tailwind/src/App.jsx
+++ 21.Tailwind/src/App.jsx
@@ -13,6 +13,11 @@ function App() {
         <div className='bg-blue-400'>child2</div>
         <div className='bg-green-400'>child3</div>
       </div>
+      <div className='grid grid-cols-12'>
+        <div className='bg-orange-400 col-span-6'>Grid1</div>
+        <div className='bg-blue-400 col-span-3'>Grid2</div>
+        <div className='bg-green-400 col-span-3'>Grid3</div>
+      </div>
     </>
   )
 }
