728d7fe52b25b4bf57dbd4c1f82a4d186f45e91b
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-06-13 23:58:19 +0530

    proceeding with blog

--- 22.Zustand-blog/src/App.jsx
+++ 22.Zustand-blog/src/App.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Counter/>
+    </div>
   )
 }
 
+function Counter(){
+  const [count, setCount] = useState(0)
+
+  return(
+    <div>
+      <CurrentCount count={count}/>
+      <br />
+      <div className='flex'>
+        <Increase setCount={setCount}/>
+        <Decrease setCount={setCount}/>
+      </div>
+    </div>
+  )
+}
+
+
+function CurrentCount({count}){
+  return(
+    <div className='text-8xl'>
+      {count}
+    </div>
+  )
+}
+
+function Decrease({setCount}){
+  return(
+    <div className='m-3'>
+      <button onClick={()=>{setCount(c=>c-1)}}>Decrease Count</button>
+    </div>
+  )
+}
+
+function Increase({setCount}){
+  return(
+    <div className='m-3'>
+      <button onClick={()=>{setCount(c=>c+1)}}>Increase Count</button>
+    </div>
+  )
+}
 export default App
