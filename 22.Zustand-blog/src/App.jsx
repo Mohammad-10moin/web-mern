90c4326373fa4a6bcc93b40d25c3307e21d37e6f
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-06-18 01:31:59 +0530

    completed blog

--- 22.Zustand-blog/src/App.jsx
+++ 22.Zustand-blog/src/App.jsx
@@ -1,3 +1,90 @@
+import './App.css'
+import { useCounter } from './store'
+
+function App() {
+  console.log("app rendering");
+  return (
+    <div>
+      <CurrentCount/>
+      <Buttons/>
+      {/* <br />
+      <div className='flex'>
+        <Increase/>
+        <Decrease/>
+      </div> */}
+
+    </div>
+  )
+}
+
+
+function CurrentCount(){
+  console.log("current component rendering");
+  const currcount=useCounter((state)=>state.count);
+  return(
+    <div className='text-8xl'>
+      {currcount}
+    </div>
+  )
+}
+
+function Buttons(){
+  console.log("Buttons rendering");
+const increase =useCounter((state)=>state.increment);
+  return (
+    <div className='flex'>
+      <div className='m-3'><button onClick={increase}>Increase</button></div>
+      <div className='m-3'><button onClick={useCounter((state)=>state.decrement)}>Decrease</button></div>
+    </div>
+  )
+}
+// function Decrease(){
+//   console.log("Decrease component rendering");
+//   const decrease=useCounter((state)=>state.decrement)
+//   return(
+//     <div className='m-3'>
+//       <button onClick={decrease}>Decrease Count</button>
+//     </div>
+//   )
+// }
+
+// function Increase(){
+//   console.log("Increase component rendering");
+//   const increase=useCounter((state)=>state.increment)
+//   return(
+//     <div className='m-3'>
+//       <button onClick={increase}>Increase Count</button>
+//     </div>
+//   )
+// }
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 import { useState } from 'react'
 import './App.css'
 
@@ -18,8 +105,7 @@ function Counter(){
       <CurrentCount count={count}/>
       <br />
       <div className='flex'>
-        <Increase setCount={setCount}/>
-        <Decrease setCount={setCount}/>
+        <Buttons setCount={setCount}/>
       </div>
     </div>
   )
@@ -34,19 +120,19 @@ function CurrentCount({count}){
   )
 }
 
-function Decrease({setCount}){
-  return(
-    <div className='m-3'>
-      <button onClick={()=>{setCount(c=>c-1)}}>Decrease Count</button>
-    </div>
-  )
-}
 
-function Increase({setCount}){
+function Buttons({setCount}){
   return(
-    <div className='m-3'>
-      <button onClick={()=>{setCount(c=>c+1)}}>Increase Count</button>
+    <div className= 'flex'>
+      <div className='m-3'>
+        <button onClick={()=>{setCount(c=>c+1)}}>Increase Count</button>
+      </div>
+      <div className='m-3'>
+        <button onClick={()=>{setCount(c=>c-1)}}>Decrease Count</button>
+      </div>
     </div>
   )
 }
 export default App
+
+*/
