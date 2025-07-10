f9d4f3363851555d7511a4551c670c8ea6186a89
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-07-10 21:00:59 +0530

    learning ...

--- 21.Tailwind/src/App.jsx
+++ 21.Tailwind/src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 import { Button } from './components/Button'
 import { InputBox } from './components/Input'
 import { Otp } from './components/Otp'
+import { SideBar } from './components/Part2'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-[#002959] h-screen flex justify-center items-center'>
-      <Otp/>
+    <div>
+      <SideBar/>
     </div>
+    // <div className='bg-[#002959] h-screen flex justify-center items-center'>
+    //   <Otp/>
+    // </div>
   )
 }
 
---
+++ 21.Tailwind/src/components/Part2.jsx
@@ -0,0 +1,6 @@
+export function SideBar(){
+    return <div className="flex">
+        <div className="bg-blue-500 h-[100vh] w-[20vw]">Sidebar</div>
+        <div className="bg-amber-400 h-[100vh] w-[80vw]">Content</div>
+    </div>
+}
