f6b7085761c1644ff3f159bc7ac5e7cd889f5077
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-07-12 20:55:58 +0530

    learnt transitions

--- 21.Tailwind/src/components/Part2.jsx
+++ 21.Tailwind/src/components/Part2.jsx
@@ -1,6 +1,6 @@
 export function SideBar(){
     return <div className="flex">
-        <div className="bg-blue-500 h-[100vh] w-[20vw]">Sidebar</div>
-        <div className="bg-amber-400 h-[100vh] w-[80vw]">Content</div>
+        <div className="transition-all bg-blue-500 h-[100vh] w-[5vw] md:w-[20vw]">Sidebar</div>
+        <div className="bg-amber-400 h-[100vh] w-full">Content</div>
     </div>
 }
